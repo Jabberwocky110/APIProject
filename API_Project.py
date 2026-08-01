23e806443e687a6faaba87e8805903072af8ea4e
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-08-01 13:21:25 -0400

    Step 1: Input

---
+++ API_Project.py
@@ -0,0 +1,16 @@
+import streamlit as st
+prompt = input("Input a general summary of the information you have researched: ")
+def question():
+ global question; input("Would you like to attach a file (yes or no)")
+ question.lower()
+def FileAttachment():
+  uploaded_file = st.file_uploader("Choose a file to attach", type=["csv", "txt", "pdf"])
+  if uploaded_file is not None:
+    # Read file bytes or pass directly to libraries like Pandas
+    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+    st.write(file_details)
+    st.success("File attached successfully!")
+question()
+while question == 'yes' or 'y':
+  FileAttachment()
+  question()
