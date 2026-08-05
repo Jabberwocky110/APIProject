6ae432348c1e8cae166a5e62db79a58812f6aaac
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-08-04 20:30:13 -0400

    ollama

--- API_Project.py
+++ API_Project.py
@@ -1,15 +1,27 @@
 import streamlit as st
-prompt = input("Input a general summary of the information you have researched: ")
-def question():
- global question; input("Would you like to attach a file (yes or no)")
- question.lower()
+import sqlite3
+import ollama
 def FileAttachment():
-  uploaded_file = st.file_uploader("Choose a file to attach", type=["csv", "txt", "pdf"])
-  if uploaded_file is not None:
+   uploaded_file = st.file_uploader("Choose a file to attach", type = ["csv", "txt", "pdf"])
+   if uploaded_file is not None:
     file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
     st.write(file_details)
     st.success("File attached successfully!")
-question()
-while question == 'yes' or 'y':
+   else:
+    print("There was an error attaching your file!")
+def question():
+ question = st.text_input("Would you like to attach a file (yes or no)")
+ question.lower()
+ if question == 'yes':
   FileAttachment()
   question()
+def TextPrompt():
+  Prompt = st.text_input("Enter your prompt or information: ")
+def Reply():
+  Reply = st.text_input("Would you like to prompt the AI?")
+  Reply.lower
+  if Reply == 'yes':
+    TextPrompt()
+summary = st.text_input("Input a general summary of the information you have researched: ")
+question()
+Reply()
